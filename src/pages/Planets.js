705b041ead2d93dc2126363bb13b35913d18a315
Natalia <onataliadelina@gmail.com>
2022-01-12 23:04:14 +0100

    show planet info

--- src/pages/Planets.js
+++ src/pages/Planets.js
@@ -1,23 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ParticlesSky from '../components/ParticlesSky'
 import planetsData from '../assets/data/planets'
 import '../css/planets.css'
+// import ModalPlanet from '../components/ModalPlanet'
 
 export default function Planets() {
     const [planets] = useState(planetsData)
+    const [planetId, setPlanetId] = useState('')
+    const [isModalOpen, setIsModalOpen] = useState(false)
 
-    const planetsTemplate = planets.map((data) => {
-        console.log("data:", data.img)
+    console.log("planetId", planetId)
+
+    // TOGGLE VIEW of PLANET DESCRIPTION
+    const toggleModal = (id) => {
+        setPlanetId(id)
+        setIsModalOpen(!isModalOpen)
+    }
+
+    let openModal = (e) => {
+        let planetId = e.target.id
+        console.log("e target", e.target.id)
+        setIsModalOpen(true)
+        setPlanetId(planetId)
+    }
+
+
+    let showPlanet = planets.map(data => {
+        if (planetId !== data.id) {
+            console.log("planetId == data.id:", planetId == data.id)
+            // <ModalPlanet data={data} />
+            return <div key={data.id}>
+                <h3>hmm</h3>
+            </div>
+        }
+        else {
+            return <div className='modal'>
+                <h3>{data.name}</h3>
+            </div>
+        }
+    })
+
+    // MAP PLANETS - DISPLAY PLANET IMAGES
+    let planetsTemplate = planets.map((data) => {
         return <div key={data.id}>
-            {/* <h3>{data.name}</h3>
-            <p>{data.description}</p> */}
-            <figure className="planet-figure">
-                <a href="#">
-                    <img src={data.img}
-                        alt={data.name} className="planet-img"
-                        width="120" />
-                </a>
-            </figure>
+            <img src={data.img}
+                alt={data.name} className="planet-img"
+                id={data.id}
+                width="120"
+                // onClick={openModal}
+                onClick={() => toggleModal(data.id)}
+            />
         </div>
     })
 
@@ -27,7 +59,10 @@ export default function Planets() {
             <div className="planets-container">
                 {planetsTemplate}
             </div>
+            {isModalOpen && planetId ?
+                <div>{showPlanet}</div>
+                : null
+            }
         </div>
     )
 }
-
