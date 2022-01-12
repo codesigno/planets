f916f6859be3a8e7aee4ab658a06eb487170c854
Natalia <onataliadelina@gmail.com>
2022-01-12 11:01:16 +0100

    remove config

--- src/pages/Planets.jsx
+++ src/pages/Planets.jsx
@@ -2,51 +2,6 @@ import React from 'react'
 import Particles from "react-tsparticles"
 
 export default function Planets() {
-    const config = {
-        showCube: false,
-        dimension: "3D",
-        velocity: 2,
-        boundaryType: "passthru",
-        antialias: false,
-        direction: {
-            xMin: -0.6,
-            xMax: 0.3,
-            yMin: -1,
-            yMax: -0.6,
-            zMin: -0.6,
-            zMax: 0.3
-        },
-        lines: {
-            colorMode: "rainbow",
-            color: "#351CCB",
-            transparency: 0.9,
-            limitConnections: true,
-            maxConnections: 20,
-            minDistance: 150,
-            visible: false
-        },
-        particles: {
-            colorMode: "solid",
-            color: "#ffffff",
-            transparency: 0.9,
-            shape: "circle",
-            boundingBox: "canvas",
-            count: 2500,
-            minSize: 1,
-            maxSize: 25,
-            visible: true
-        },
-        cameraControls: {
-            enabled: true,
-            enableDamping: true,
-            dampingFactor: 0.2,
-            enableZoom: true,
-            autoRotate: false,
-            autoRotateSpeed: 0.3,
-            resetCameraFlag: true
-        }
-    };
-
     const particlesInit = (main) => {
         console.log(main);
     };
@@ -56,7 +11,7 @@ export default function Planets() {
     };
 
     return (
-        <div id="planets-particles">
+        <div id="planets-particles-container">
             <Particles
                 id="tsparticles"
                 init={particlesInit}
