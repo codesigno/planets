77f661bddf539308944766ac9a136aca9deffe62
Natalia <onataliadelina@gmail.com>
2022-01-15 18:41:55 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
+    const [navbarToggle, setNavbarToggle] = useState(false)
+
     return (
         <nav>
             <div className="nav">
                 <Link to="/"> <i className="fas fa-satellite icon"></i></Link>
-                <Link to="/" className="menu menu-icon"><span></span></Link>
+                <Link to="/" className="menu menu-icon" onClick={() => { setNavbarToggle((navbarToggle) => !navbarToggle) }}><span></span></Link>
             </div>
-            {/* <div className="hidden-menu">
-                <Link to="/" className="nav-link">home</Link>
-                <Link to="/space" className="link">space</Link>
-                <Link to="/#planet" className="link">planets</Link>
-            </div> */}
+            {navbarToggle ?
+                <div onMouseLeave={() => { setNavbarToggle(false) }} className='menu-hidden'>
+                    <Link to="/" className="nav-link">home</Link>
+                    <Link to="/space" className="link">space</Link>
+                    <Link to="/#planet" className="link">planets</Link>
+                </div>
+                : null}
         </nav>
     )
 }
