f279563a95fdd6f34c92a607c7d52d9fcd459918
Natalia <onataliadelina@gmail.com>
2022-01-15 18:42:14 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import './css/App.css';
 import './css/nav.css';
-// import SolarSystem from './components/SolarSystem';
-// import Planets from './pages/Planets';
 import Header from './components/Header';
 import RoutesNavBar from './RoutesNavBar'
-
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        {/* <SolarSystem /> */}
-        {/* <Home /> */}
-        {/* <Planets /> */}
-        {/* <Navbar /> */}
         <RoutesNavBar />
       </BrowserRouter>
     </div>
